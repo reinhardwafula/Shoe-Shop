c7bc39fd431b28408f0305137102f2a4f923c3fe
reinhardwafula <rehynsanders@gmail.com>
2021-11-16 15:53:28 +0300

    Monospace

--- app/src/main/java/com/example/mobileshop/SplashActivity.kt
+++ app/src/main/java/com/example/mobileshop/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.example.mobileshop
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
